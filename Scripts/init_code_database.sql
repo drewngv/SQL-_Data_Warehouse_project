8dc0028486c7beb8740b1a6d8bc1f1847a5f829d
drewngv <105620129+drewngv@users.noreply.github.com>
2025-04-13 11:04:02 +0700

    Update init_code_database.sql

--- Scripts/init_code_database.sql
+++ Scripts/init_code_database.sql
@@ -1,1 +1,22 @@
+Use master;
+go
 
+if exists ( select 1 from sys.database where name='Datawarehouse')
+Begin 
+  alter DATABASE Datawarehouse set singler_user with rollback immediate;
+  drop DATABASE Datawarehouse;
+end;
+go
+
+Create DATAEBASE Datawarehouse;
+go
+
+use Datawarehouse;
+go
+
+create schema bronze;
+go 
+create schema siler;
+go
+create schema gold;
+go
